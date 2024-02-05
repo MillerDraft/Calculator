e72d451ca8cfa5b21ff5a9e3db8dbca5a420fe3e
Osmany <osmanysalsero@gmail.com>
2024-02-04 17:55:18 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        StandardCal stand = new StandardCal();
+        System.out.println("........................");
+
+        RomanCal romana = new RomanCal();
+        System.println(".............................");
+
+        BinaryCal bina = new BinaryCal();
+        System.out.println("End");
+    }
+}
